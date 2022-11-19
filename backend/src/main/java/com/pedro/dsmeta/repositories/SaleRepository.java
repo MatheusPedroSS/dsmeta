d78b7221b7d0e64b73ba9a92bb5ac545a1ae9c67
Matheus Pedro <matheus.pedro1214@gmail.com>
2022-11-18 22:39:06 -0300

    Date select

--- backend/src/main/java/com/pedro/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/pedro/dsmeta/repositories/SaleRepository.java
@@ -1,5 +1,9 @@
 package com.pedro.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,6 @@ import com.pedro.dsmeta.entities.Sale;
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
     
+    Page<Sale> findByDateBetweenOrderByAmountDesc(LocalDate min, LocalDate max, Pageable pageable);
+
 }
--- backend/src/main/java/com/pedro/dsmeta/servicies/SaleService.java
+++ backend/src/main/java/com/pedro/dsmeta/servicies/SaleService.java
@@ -1,8 +1,12 @@
 package com.pedro.dsmeta.servicies;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.pedro.dsmeta.entities.Sale;
@@ -14,8 +18,18 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sale> findSales() {
-        return repository.findAll();
+    public Page<Sale> findSales(Pageable pageable, String minDate, String maxDate) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("")
+                ? today.minusYears(1L)
+                : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("")
+                ? today
+                : LocalDate.parse(maxDate);
+
+        return repository.findByDateBetweenOrderByAmountDesc(min, max, pageable);
     }
 
 }
